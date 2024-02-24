b33fca9dc6d44e2fc07e4debced4a2dbd789bd8e
Arihant Kamde <kamdearihant01@gmail.com>
2024-02-25 02:34:57 +0530

    login ui update

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,41 +8,58 @@ import {
 } from "../errors/customErrors.js";
 
 export const register = async (req, res) => {
-  const { name, email, password, selectedTopics } = req.body;
-  req.body.password = await hashPassword(password);
-
-  const user = await User.create({
+  const {
     name,
     email,
-    password: req.body.password,
-    interest: selectedTopics,
-  });
-  res.status(StatusCodes.CREATED).json({ message: "User registered." });
+    password,
+    selectedTopics,
+    dateOfBirth,
+    profession,
+    gender,
+  } = req.body;
+  req.body.password = await hashPassword(password);
+
+  // const user = await User.create({
+  //   name,
+  //   email,
+  //   password: req.body.password,
+  //   interest: selectedTopics,
+  //   dateOfBirth,
+  //   profession,
+  //   gender,
+  // });
+  // res.status(StatusCodes.CREATED).json({ message: "User registered." });
+  console.log(name, email, password, dateOfBirth, profession, gender);
 };
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-  console.log(email);
-  console.log(password);
-  const userExist = await User.findOne({ email });
+  // const userExist = await User.findOne({ email });
 
-  if (!userExist) {
-    throw new BadRequestError("User does not exist.");
-  }
+  // if (!userExist) {
+  //   throw new BadRequestError("User does not exist.");
+  // }
 
-  const validatePassword = await comparePassword(password, userExist.password);
-  console.log(validatePassword);
-  if (!validatePassword) {
-    throw new UnauthenticatedError("Invalid credentials.");
-  }
+  // const validatePassword = await comparePassword(password, userExist.password);
+  // if (!validatePassword) {
+  //   throw new UnauthenticatedError("Invalid credentials.");
+  // }
 
-  const token = createJWT({ id: userExist._id, user: userExist.name });
+  // const token = createJWT({ id: userExist._id, user: userExist.name });
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
-    secure: process.env.NODE_ENV === "production",
-  });
+  // res.cookie("token", token, {
+  //   httpOnly: true,
+  //   expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+  //   secure: process.env.NODE_ENV === "production",
+  // });
+  // res.status(StatusCodes.OK).json({
+  //   id: userExist._id,
+  //   name: userExist.name,
+  //   message: "User logged in.",
+  // });
+};
 
-  res.status(StatusCodes.OK).json({ message: "User logged in." });
+export const getUser = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
 };
